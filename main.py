fdfc4c73b02493794620ec39cf236d71d860c4f7
youngmoon1116 <86809209+youngmoon1116@users.noreply.github.com>
2021-07-02 08:36:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from downloader import Downloader
 settings = {
     # Manga urls, should be the same website
     'manga_url': [
-        'URL_1',
+        'https://member.bookwalker.jp/app/03/webstore/cooperation?r=BROWSER_VIEWER/aabd4c95-83d4-4ddc-b2b6-bdcedf60628e/https%3A%2F%2Fbookwalker.jp%2FholdBooks%2F&_ga=2.107523970.140572903.1625176282-1885277533.1622811587',
         'URL_2'
     ],
     # Your cookies
-    'cookies': 'YOUR_COOKIES_HERE',
+    'cookies': 'krt.__rnd=74; _fbp=fb.1.1622811590324.1230290157; _ts_yjad=1622811591555; cookie_optin=1; _gcl_au=1.1.1906906059.1625176282; _gid=GA1.2.140572903.1625176282; cm_kp_login_account=GL; myStore=0; showSlider=0; bweternity=1aqfs7otdnhqr3di1hgdh50pjfbpvpibg9l1uqps9bkjvstq; holdBookRequestParams=%7B%22st%22%3A0%2C%22cat%22%3A0%2C%22com%22%3A0%2C%22lab%22%3A0%2C%22rf%22%3A0%2C%22r18ex%22%3A0%2C%22order%22%3A0%2C%22list%22%3A0%2C%22kw%22%3Anull%2C%22limit%22%3A50%2C%22page%22%3A1%7D; bwmember=U2FsdGVkX19oZW5seUJLV3D2DQIU1T8DW1uRgAfsPS8%3D; bwsess=9d4grf09ldfkstevcqgrq01llq; lbwsid=3010b9cacbd307fa2c85e76bb3fe8fabe6190bcd3f0f2b7ccebd49768248f3c1; AMP_TOKEN=%24NOT_FOUND; _ga=GA1.2.1885277533.1622811587; _ga_EPKMR4CGNW=GS1.1.1625176281.2.1.1625179895.47; _gat=1',
     # Folder names to store the Manga, the same order with manga_url
     'imgdir': [
         'IMGDIR_FOR_URL_1',
